8ebe33c88740a045781565aad8b4dda21e5babb8
MironBerch <mironberchetov@gmail.com>
2022-11-25 21:17:26 +0300

    fast-auth

---
+++ fast_auth/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.contrib import messages
+
+
+
+def signup(request):
+    pass
+
+
+def signin(request):
+    pass
+
+
+def logout(request):
+    pass
